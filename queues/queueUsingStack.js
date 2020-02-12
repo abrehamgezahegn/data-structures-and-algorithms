44b8147f172ced9f374c2f697e07e18002cc7457
abreham gezahegn <abreham0913@gmail.com>
2020-02-12 14:39:42 +0300

    queue using array re-implemented

--- queues/queueUsingStack.js
+++ queues/queueUsingStack.js
@@ -1,6 +1,34 @@
+const { stackOne, stackTwo } = require("../stacks/stackArray");
+
 class Queue {
   constructor() {
-    this.first = null;
-    this.last = null;
+    this.queue = stackOne;
+  }
+
+  enqueue(value) {
+    this.queue.push(value);
+  }
+
+  dequeue() {
+    let item;
+    if (stackTwo.stack.length === 0) {
+      let counter = this.queue.stack.length;
+      while (counter > 0) {
+        stackTwo.push(this.queue.pop());
+        counter--;
+      }
+    }
+
+    item = stackTwo.pop();
+    console.log(item);
   }
 }
+
+const myQueue = new Queue();
+
+myQueue.enqueue("highpower");
+myQueue.enqueue("stand");
+myQueue.enqueue("for");
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.enqueue("oblivion");
